24144ca210493be14e343ad21c8baa8dd5d87113
mayumi-hagiwara <45552959+mayumi-hagiwara@users.noreply.github.com>
2019-02-21 19:16:32 +0900

    プロジェクト ファイルを追加します。

---
+++ benkyouyou_0221/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="benkyouyou_0221.MvcApplication" Language="C#" %>
---
+++ benkyouyou_0221/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(benkyouyou_0221.Startup))]
+namespace benkyouyou_0221
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
